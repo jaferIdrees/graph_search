cc8aea7aa0b67527db42cc40f88042eef848431e
jaferidrees <jaferll@gmail.com>
2023-01-25 18:04:39 +0100

    initial commit

---
+++ graph.js
@@ -0,0 +1,23 @@
+const airports = 'PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM'.split(' ');
+
+const routes = [
+    ['PHX', 'LAX'],
+    ['PHX', 'JFK'],
+    ['JFK','OKC'],
+    ['JFK','HEL'],
+    ['JFK','LOS'],
+    ['MEX','LAX'],
+    ['MEX','BKK'],
+    ['MEX','LIM'],
+    ['MEX','EZE'],
+    ['LIM','BKK']
+]
+
+//The graph
+const adjacencyList = new Map();
+
+//Add node
+const addNode = (airport) => {
+    adjacencyList.set(airport, []);
+}
+
